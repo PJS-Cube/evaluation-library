9c9d7d4a407aecdac3852ae34451b6c42fbabbac
Herbert Hart <hart_herbert@hotmail.com>
2019-10-21 13:20:22 +0200

    :wrench: added line item base

---
+++ lib/models/line.item.base.d.ts
@@ -0,0 +1,15 @@
+import { IUser } from "./user";
+import { IClient } from "./client";
+import { IBaseDocument } from "./base.document";
+export interface ILineItemBase {
+    Oid?: string;
+    CreatedOn?: Date;
+    ModifiedOn?: Date;
+    CreatedBy?: IUser;
+    ModifiedBy?: IUser;
+    Client?: IClient;
+    ObjectType?: number;
+    Text?: string;
+    LongText?: string;
+    Document?: IBaseDocument;
+}
